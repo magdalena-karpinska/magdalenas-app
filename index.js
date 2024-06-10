166ca33995a7b166e9f71f66a2866fed73efaf17
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-06-10 10:02:52 +0200

    Adds the name flag

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const { Command } = require("commander");
 const program = new Command();
 
-program.version("1.0.0.").description("My own CLI");
+program.version("1.0.0").description("My own CLI");
 
 program
-  .command("Hello!")
-  .description("Respond with a greeting")
-  .action(() => {
-    console.log("Hello SALT Developer")
+  .command("Hello")
+  .description("Responds with a greeting")
+  .option("-n, --name <type>", "Add your name")
+  .action((user) => {
+    const name = user.name ? user.name : "stranger danger!";
+    console.log(`Hello SALT Developer: ${name}!`)
   });
 
-  program.parse(process.argv)
+program.parse(process.argv);

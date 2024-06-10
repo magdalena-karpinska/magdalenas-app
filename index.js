123ae6650d5d345187a808c64a7dc646438184d7
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-06-10 10:26:14 +0200

    Cleans the code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ program.version("1.0.0").description("My own CLI");
 program
   .command("Hello")
   .description("Responds with a greeting")
-  .option("-n, --name <type>", "Add your name")
+  .option("-n, --name <type>", "If you're a SALT Developer, add your name")
   .action((user) => {
     const name = user.name ? user.name : "stranger danger!";
     console.log(`Hello SALT Developer: ${name}!`)
